1a6838aab73a2e973a691af4022e400b52a1eea0
Ashwin Mahadevan <mahadevan@ashwinm.com>
2023-06-17 16:48:21 -0700

    push

--- server/main.py
+++ server/main.py
@@ -1,7 +1,7 @@
 import sqlite3
 import datetime
 
-con = sqlite3.connect("/Users/arvind/Library/Messages/chat.db")
+con = sqlite3.connect("/Users/ashwin/Library/Messages/chat.db")
 cur = con.cursor()
 
 def nanoseconds_in_years(years):
@@ -24,7 +24,7 @@ offset_time_ns = current_time_ns - year31_nanoseconds - (1*24*60*60*1e9)
 
 res = cur.execute(f"SELECT * FROM message WHERE is_read = 0 AND is_from_me = 0 AND text is NOT NULL AND date >= {offset_time_ns} ORDER BY date DESC LIMIT 3")
 
-print(res.fetchall())
+#print(res.fetchall())
 
 #res2 = cur.execute("SELECT text FROM message WHERE cache_roomnames = 'chat107061102355140117' ")
 """
@@ -32,3 +32,14 @@ select chat.display_name from message.cache_roomnames join with chat.room_name -
 select handle.id from message.handle_id join with handle.ROW_ID --> Phone Number/Email
 """
 #print(res2.fetchall())
+
+res3 = cur.execute(f"SELECT text, handle.id FROM message JOIN handle ON message.handle_id = handle.ROWID WHERE message.date >= {offset_time_ns}")
+#print(res3.fetchall())
+
+s = ""
+
+for text, id in res3:
+
+    s += f'{id}: {text}\n'
+
+print(s)
