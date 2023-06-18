ab0683b7e919e9eb8f65c5941f894e4ab9d4206e
Shrey Pandya <shrey150@yahoo.com>
2023-06-17 20:45:20 -0700

    add summary component

---
+++ client/src/components/Summary.tsx
@@ -0,0 +1,13 @@
+type SummaryProps = {
+    summary: string[]
+}
+
+const Summary = ({ summary }: SummaryProps) => (
+    <ul className="list-disc p-4">
+        {summary.map((message, i) => (
+          <li key={i}>{message}</li>
+        ))}
+    </ul>
+)
+
+export default Summary
