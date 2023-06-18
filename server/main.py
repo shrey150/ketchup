6b995c5aa6ab75875b5add4cd7f417574a63a862
Arvind Vivekanandan <arvind.v@berkeley.edu>
2023-06-17 21:38:05 -0700

    flask complete

--- server/main.py
+++ server/main.py
@@ -5,7 +5,7 @@ import llm
 
 app = Flask(__name__)
 
-@app.get("/topics")
+@app.get("/api/topics")
 def get_topics():
     messages = db.get_messages(days_ago=1)
     topics = llm.generate_topics(messages)
@@ -17,10 +17,12 @@ def get_topics():
         summary = llm.generate_summary(topic_messages_dict)
         bullets = llm.generate_bullets(topic_messages_dict)
         topic_messages = []
+        dates = []
         for ID in topic_messages_dict:
             text, date, handle_id, display_name = topic_messages_dict[ID]
+            dates.append(date)
             topic_messages.append({"groupName": display_name, "senderName": handle_id, "text": text, "timestamp": date})
-        reponse.append({"id": topic_id, "emoji": emoji, "name": title, "description": summary, "messageCount": len(IDs), "summary": bullets, "updatedAt": min(topic_messages, key=lambda msg: msg[2])[2], "messages": topic_messages})
+        reponse.append({"id": topic_id, "emoji": emoji, "name": title, "description": summary, "messageCount": len(IDs), "summary": bullets, "updatedAt": min(dates), "messages": topic_messages})
         topic_id += 1
     return reponse
 
