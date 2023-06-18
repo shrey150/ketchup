7d910fcc8f0e8202cb858728893ea65b1c7041ce
Shrey Pandya <shrey150@yahoo.com>
2023-06-17 23:51:55 -0700

    [WIP] test

--- server/main.py
+++ server/main.py
@@ -21,29 +21,30 @@ def ping():
 
 @app.get("/api/topics")
 def get_topics():
-    print('Fetching messages')
-    messages = db.get_messages(days_ago=1)
-    print('Generating topics')
-    topics = llm.generate_topics(messages)
-    print('Got topics!')
-    pprint(topics)
-    response = []
-    topic_id = 0
-    for emoji, title, IDs in topics:
-        topic_messages_dict = {ID: messages[ID] for ID in IDs}
-        print('Generating summary')
-        summary = llm.generate_summary(topic_messages_dict)
-        print('Generating bullets')
-        bullets = llm.generate_bullets(topic_messages_dict)
-        topic_messages = []
-        dates = []
-        for ID in topic_messages_dict:
-            text, date, handle_id, display_name = topic_messages_dict[ID]
-            dates.append(date)
-            topic_messages.append({"groupName": display_name, "senderName": handle_id, "text": text, "timestamp": date})
-        response.append({"id": topic_id, "emoji": emoji, "name": title, "description": summary, "messageCount": len(IDs), "summary": bullets, "updatedAt": min(dates), "messages": topic_messages})
-        topic_id += 1
-    print('Returning topics')
+    # print('Fetching messages')
+    # messages = db.get_messages(days_ago=1)
+    # print('Generating topics')
+    # topics = llm.generate_topics(messages)
+    # print('Got topics!')
+    # pprint(topics)
+    # response = []
+    # topic_id = 0
+    # for emoji, title, IDs in topics:
+    #     topic_messages_dict = {ID: messages[ID] for ID in IDs}
+    #     print('Generating summary')
+    #     summary = llm.generate_summary(topic_messages_dict)
+    #     print('Generating bullets')
+    #     bullets = llm.generate_bullets(topic_messages_dict)
+    #     topic_messages = []
+    #     dates = []
+    #     for ID in topic_messages_dict:
+    #         text, date, handle_id, display_name = topic_messages_dict[ID]
+    #         dates.append(date)
+    #         topic_messages.append({"groupName": display_name, "senderName": handle_id, "text": text, "timestamp": date})
+    #     response.append({"id": topic_id, "emoji": emoji, "name": title, "description": summary, "messageCount": len(IDs), "summary": bullets, "updatedAt": min(dates), "messages": topic_messages})
+    #     topic_id += 1
+    # print('Returning topics')
+    return "hi"
     return response
 
 
