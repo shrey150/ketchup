60cb3421bd1c09ed32fe4650ed64f4366fa6aca4
Arvind Vivekanandan <arvind.v@berkeley.edu>
2023-06-17 17:17:00 -0700

    days delta change

--- server/main.py
+++ server/main.py
@@ -1,7 +1,7 @@
 import sqlite3
 import datetime
 
-con = sqlite3.connect("/Users/ashwin/Library/Messages/chat.db")
+con = sqlite3.connect("/Users/arvind/Library/Messages/chat.db")
 cur = con.cursor()
 
 def nanoseconds_in_years(years):
@@ -19,12 +19,14 @@ def nanoseconds_in_years(years):
 # Calculate the number of nanoseconds in 31 years
 year31_nanoseconds = nanoseconds_in_years(31)
 current_time = datetime.datetime.now()
-current_time_ns = current_time.microsecond * 1000
+current_time_ns = current_time.timestamp() * 1e9
 offset_time_ns = current_time_ns - year31_nanoseconds - (1*24*60*60*1e9)
+days_ago_delta = 1
+days_ago = current_time - datetime.timedelta(days=days_ago_delta)
 
-res = cur.execute(f"SELECT * FROM message WHERE is_read = 0 AND is_from_me = 0 AND text is NOT NULL AND date >= {offset_time_ns} ORDER BY date DESC LIMIT 3")
+res = cur.execute(f"SELECT text, datetime(date/1000000000 + 978307200,'unixepoch','localtime') FROM message WHERE is_read = 0 AND is_from_me = 0 AND text is NOT NULL AND datetime(date/1000000000 + 978307200,'unixepoch','localtime') >= '{days_ago}' ORDER BY date DESC")
 
-#print(res.fetchall())
+print(res.fetchall())
 
 #res2 = cur.execute("SELECT text FROM message WHERE cache_roomnames = 'chat107061102355140117' ")
 """
@@ -33,12 +35,12 @@ select handle.id from message.handle_id join with handle.ROW_ID --> Phone Number
 """
 #print(res2.fetchall())
 
-res3 = cur.execute(f"SELECT text, handle.id FROM message JOIN handle ON message.handle_id = handle.ROWID WHERE message.date >= {offset_time_ns}")
+res3 = cur.execute(f"SELECT text, date, handle.id FROM message JOIN handle ON message.handle_id = handle.ROWID WHERE datetime(date/1000000000 + 978307200,'unixepoch','localtime') >= '{days_ago}'")
 #print(res3.fetchall())
 
 s = ""
 
-for text, id in res3:
+for text, date, id in res3:
 
     s += f'{id}: {text}\n'
 
