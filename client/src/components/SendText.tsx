00ba6001f07344d7d89a1021c20845c33ce4033b
Shrey Pandya <shrey150@yahoo.com>
2023-06-18 16:12:52 -0700

    fix typing in message box

--- client/src/components/SendText.tsx
+++ client/src/components/SendText.tsx
@@ -1,5 +1,5 @@
 import { useKetchupState } from '@/services/KetchupState'
-import React from 'react'
+import React, { useState } from 'react'
 import {HiOutlineSparkles} from 'react-icons/hi'
 import {BsSend} from 'react-icons/bs'
 
@@ -12,15 +12,26 @@ const SendText = ({textResponse, sender}: sendTextProp) => {
 
     const state = useKetchupState();
 
+    const [userResponse, setUserResponse] = useState(textResponse);
+    const handleChange = (e: any) => setUserResponse(e.target.value);
+
+    const handleClick = (e: any) => {
+        e.stopPropagation();
+        state.sendMessage(textResponse, sender);
+        alert('Message sent!');
+    }
+
     return (
         <div className="flex flex-row items-center justify-between bg-rose-100 p-2 rounded">
             <HiOutlineSparkles />
-            <input className="overflow-auto flex-grow px-4 rounded-lg mx-2" type="Text" value={textResponse}></input>
-            <button onClick={(e) => {
-                e.stopPropagation();
-                state.sendMessage(textResponse, sender)
-                alert('Message sent!')
-            }}>
+            <input
+                className="overflow-auto flex-grow px-4 rounded-lg mx-2"
+                type="text"
+                value={userResponse}
+                onClick={(e) => e.stopPropagation()}
+                onChange={handleChange}
+            />
+            <button onClick={handleClick}>
                 <BsSend />
             </button>
         </div>
